7fa995ad1436256f85db784c37aec2ce8b469b73
SA <62649689+scottieyarn1135@users.noreply.github.com>
2025-03-07 17:57:34 +0000

    added https to follow best practices

--- api_request.py
+++ api_request.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 steam_key = os.getenv("STEAM_KEY")
 
-request = requests.get(f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={steam_key}&steamid=76561197960434622&format=json")
+request = requests.get(f"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={steam_key}&steamid=76561197960434622&format=json")
 
 print(request)
