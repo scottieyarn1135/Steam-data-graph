b8ec95975710a73af562d507c0e4bbc7980fb962
pandapanda135 <pandas135works@gmail.com>
2025-03-07 17:54:22 +0000

    add api request base

---
+++ api_request.py
@@ -0,0 +1,11 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+steam_key = os.getenv("STEAM_KEY")
+
+request = requests.get(f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={steam_key}&steamid=76561197960434622&format=json")
+
+print(request)
